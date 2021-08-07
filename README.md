e234fd7a5408b50049ae2f3a4768b30e5880492b
Nicholas Fraser <nick@ludocode.com>
2021-08-06 23:31:12 -0400

    Clean up configuration of stdlib functions
    These are now configured as e.g. MPACK_MEMCMP, not mpack_memcmp.

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
-This builds the [MPack](https://github.com/ludocode/mpack) unit test suite into a Linux kernel module.
+# MPack in the Linux Kernel
 
-## Using MPack in the Linux kernel
+[MPack](https://github.com/ludocode/mpack) is a C implementation of an encoder and decoder for the [MessagePack](http://msgpack.org/) serialization format.
 
-The file [`mpack-config.h`](mpack-config.h) provides an example configuration to use to get MPack working in the Linux kernel. To use it, copy it into your include path and define `-DMPACK_HAS_CONFIG=1` when building MPack.
+This repository contains an example [`mpack-config.h`](mpack-config.h) configuration file to use to build MPack in the Linux kernel. To use it, copy it into your include path and define `-DMPACK_HAS_CONFIG=1` when building MPack.
 
 This configuration is not done automatically by MPack because including Linux headers would make MPack a derived work of Linux requiring dual-licensing it under the GPLv2. Instead this code is kept separate so that only this config and test shim is dual-licensed.
 
 ## Building and running the unit tests
 
-You will need the Linux headers installed for your kernel. First clone or symlink the MPack tree into this folder, then type `make` to build the unit test kernel module:
+This repository contains a unit test shim that builds the MPack unit test suite into a Linux kernel module. You do not need to build the unit tests to use MPack in the kernel.
+
+To build the unit tests, you will need the Linux headers installed for your kernel. First clone or symlink the MPack tree into this folder, then type `make` to build the unit test kernel module. For example:
 
 ```
+git clone https://github.com/ludocode/mpack-linux-kernel.git
+cd mpack-linux-kernel
 git clone https://github.com/ludocode/mpack.git
 make
 ```
 
-You can then run the unit tests by inserting the `mpack-tests.ko` kernel module. Beware, if a test fails the kernel may panic, your data may be destroyed, your machine may catch fire etc. Run this in a virtual machine.
+You can then run the unit tests by inserting the `mpack-tests.ko` kernel module. Beware, if a test fails or if bugs occur the kernel may panic, your data may be destroyed, your machine may catch fire etc. Run this in a virtual machine.
 
 ```
 sudo insmod mpack-tests.ko  # DANGER
--- mpack-config.h
+++ mpack-config.h
@@ -7,24 +7,25 @@
 // This is an example mpack-config.h for compiling MPack v1.1 in the Linux kernel.
 //     See: https://github.com/ludocode/mpack
 
-// These headers are required to provide the necessary types and functions that
-// would normally be provided by libc or by a conforming C99 compiler.
+// The linux kernel doesn't have proper headers like <limits.h> or
+// <inttypes.h>. Since this doesn't conform to the C spec (not even for
+// freestanding implementations), we turn off MPACK_CONFORMING. This also
+// disables MPACK_STDLIB, MPACK_STDIO and some other stuff.
 #define MPACK_CONFORMING 0
+
+// Since we don't have the stdlib or freestanding headers, MPack won't include
+// any. We have to include the Linux headers to at least define the standard
+// fixed-width integer types as well as size_t, bool, and so on.
 #include <linux/types.h>
-#include <linux/limits.h>
 
-// Floating point is not supported in the kernel.
-#define MPACK_FLOAT 0
-
-// Even though we don't have a real stdlib or conforming headers, we do have
-// the standard string functions in a Linux-specific header. This forces MPack
-// to use them.
+// We do still have the standard string functions but they're in a
+// Linux-specific header. We configure MPack to use them.
 #include <linux/string.h>
-#define mpack_memcmp memcmp
-#define mpack_memcpy memcpy
-#define mpack_memmove memmove
-#define mpack_memset memset
-#define mpack_strlen strlen
+#define MPACK_MEMCMP memcmp
+#define MPACK_MEMCPY memcpy
+#define MPACK_MEMMOVE memmove
+#define MPACK_MEMSET memset
+#define MPACK_STRLEN strlen
 
 // This makes MPack use kmalloc() for allocations, allowing you to use things
 // like the growable writer or the dynamic tree parser. It's probably not a
@@ -37,11 +38,14 @@
 #define MPACK_FREE(x) kfree(x)
 #endif
 
+// Floating point is not supported in the kernel.
+#define MPACK_FLOAT 0
+
 // This configuration turns debugging off; we don't want to abort on errors. If
 // you want you could instead define an MPACK_CUSTOM_ASSERT function that
 // triggers a kernel panic with a friendly error message, or printk()s the
-// error and goes into an infinite loop. You probably won't be able to compile
-// the unit test suite with this on though.
+// error and stops somehow. You probably won't be able to compile the unit test
+// suite with this on.
 #define MPACK_DEBUG 0
 
 // Tracking should probably be off (in case you enable allocations) since it is
